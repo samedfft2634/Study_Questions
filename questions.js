2a61d28d6d16c4953f42ec4a4271f67b960cb898
samedfft2634 <samedfft@gmail.com>
2023-10-11 18:36:43 +0300

    third question

--- questions.js
+++ questions.js
@@ -31,5 +31,18 @@ console.log(`exponent function : ${exponent(2,4)}`)
 console.log(`exponent function : ${exponent(5,4)}`)
 console.log(`exponent function : ${exponent(7,4)}`)
 
+//^ -------------------------------------------------------------------------- */
+//^                                 Question 3                                 */
+//^ -------------------------------------------------------------------------- */
+//+ QUESTION3: Write a function that takes the year as a parameter and returns the result to the main program by calculating whether this year is a leap year or not using the function-expression method.
 
+const leapYear = function(x) {
+    year = prompt("Enter the year :")
+     if( year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
+        console.log(`${year} is a leap year.`)
+     } else {
+        console.log(`${year} is not a leap year.`)
+     }
+}
 
+console.log(leapYear())
