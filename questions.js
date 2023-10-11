542dc971698dc6168d04b9f522f75bb70199756b
samedfft2634 <samedfft@gmail.com>
2023-10-11 18:14:01 +0300

    first quesiton

---
+++ questions.js
@@ -0,0 +1,9 @@
+//^ -------------------------------------------------------------------------- */
+//^                                 Question 1                                 */
+//^ -------------------------------------------------------------------------- */
+//+ QUESTION1: Write a function that takes the base and height as parameters and returns the area of a triangle to the main program.
+function areaOfTriangle (base, height) {
+    return (base * height) / 2;
+}
+
+console.log(areaOfTriangle(5,10))
